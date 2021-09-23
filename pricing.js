070842bfde3b9c6b04ecd910d6d68ba6cf5bccec
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2021-09-23 15:08:31 +0300

    changes

--- pricing.js
+++ pricing.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
     });
     $("button#send-text").click(function() {
         let clientName = $("input#client-name").val();
-        alert("Hello! our dear loyal customer:" + "" + clientName + "" + "your total cost for designing cost Ksh.260000/$.2600");
+        swal("Hello " + clientName + ", our dear loyal customer, your total  designing cost: Ksh.260000/$.2600");
 
 
 
@@ -14,7 +14,7 @@ $(document).ready(function() {
         $("#payment-form").show();
     });
     $("button#btn-pay").click(function() {
-        alert("We have received your payments and we will get back to you as soon as possible. Thank you for working with us.");
+        swal("We have received your payments and we will get back to you as soon as possible. Thank you for working with us.");
     });
 
 });
